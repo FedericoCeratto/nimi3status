7616b84495bb8beda37883c5e15f2de5efd25be8
Federico Ceratto <federico.ceratto@gmail.com>
2015-12-22 23:23:05 +0000

    Add readline file

---
+++ readline_timeout.nim
@@ -0,0 +1,27 @@
+#
+# Nim i3 status bar - readline with timeout
+#
+
+from posix import Timeval, select, TFdSet, FD_SET, FD_ZERO
+
+proc timeValFromMilliseconds(timeout): Timeval =
+  let seconds = timeout div 1000
+  result.tv_sec = seconds.int32
+  result.tv_usec = ((timeout - seconds * 1000) * 1000).int32
+
+proc readLine*(f: File, timeout:int): TaintedString =
+  result = ""
+  var rd: TFdSet
+  FD_SET(f.getFileHandle, rd)
+
+  var tv = timeValFromMilliseconds(timeout)
+  var data_available = int(select(cint(f.getFileHandle+1), addr(rd), nil, nil, addr(tv)))
+  if data_available == 0:
+    return nil
+
+  # read until newline
+  while true:
+    let c = f.readChar()
+    if c == '\x0A':
+      return result
+    result.add(c)
